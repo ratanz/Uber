d82455e2df4a1c278c0724747903fe0db1764bda
ratanz <ratanrathod870@gmail.com>
2025-01-07 16:49:55 +0530

    ui changes and v1 of frontend side of things

--- frontend/src/components/FinishRide.jsx
+++ frontend/src/components/FinishRide.jsx
@@ -1,24 +1,16 @@
-import React, {useState}from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import FinishRide from './FinishRide'
 
-const ConfirmRidePopup = (props) => {
-
-    const [otp, setOtp] = useState("")
-
-    const sumbitHandler = (e) => {
-        e.preventDefault()
-    }
-
+const FinishRide = (props) => {
     return (
-        <div>
+        <div className='h-full'>
             <h5 onClick={() => {
-                props.setConfirmRidePopupPanel(false)
+                props.setFinishRidePanel(false)
             }} className='p-1 text-center absolute top-0 w-[93%]'> <i className="ri-arrow-down-wide-line text-3xl text-gray-400 "></i></h5>
 
-            <h2 className='text-2xl font-bold mb-5'>Confirm this ride to start!</h2>
+            <h2 className='text-2xl font-bold mb-5'>Finish this Ride!</h2>
 
-            <div className='flex items-center justify-between p-3 bg-yellow-400 rounded-lg mt-6'>
+            <div className='flex items-center justify-between p-4 border-2 border-yellow-400/70 rounded-lg mt-6'>
                 <div className='flex items-center gap-2'>
                     <img className='h-12 w-12 rounded-full object-cover' src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHBvcnRyYWl0JTIwbWVufGVufDB8fDB8fHww" alt="" />
                     <h2 className='text-lg font-medium'>Ratan Rathod</h2>
@@ -28,7 +20,7 @@ const ConfirmRidePopup = (props) => {
 
             <div className='flex flex-col justify-between items-center gap-3'>
 
-                <div className='w-full mt-5 '>
+                <div className='w-full mt-6 '>
 
                     <div className='flex items-center p-3 border-b-2 gap-4 my-2'>
                         <i className="text-lg ri-map-pin-user-fill"></i>
@@ -57,28 +49,22 @@ const ConfirmRidePopup = (props) => {
 
                 </div>
 
-                <div className='button-container w-full '>
-                    <form className='flex flex-col items-center justify-center gap-2 p-4 rounded-xl' onSubmit={(e) => {
-                        sumbitHandler(e)
-                    }}>
-                        <input
-                            value={otp}
-                            onChange={(e) => {
-                                setOtp(e.target.value)
-                            }} type="text" placeholder='Enter OTP' className='bg-[#eee] px-6 py-4 text-lg rounded-lg font-mono md:w-1/2 w-full ' />
+                <div className='button-container w-full flex flex-col items-center justify-center '>
 
-                        <Link to='/captain-riding' onClick={() => {
+                    <Link
+                        to='/captain-riding'
+                        onClick={() => {
                             props.setConfirmRidePopupPanel(true)
                             props.setRidePopupPanel(false)
                         }}
-                            className='bg-green-500 text-center text-lg md:w-1/2 w-full font-semibold text-white px-10 py-2 rounded-lg'>Confirm</Link>
+                        className='bg-green-500 text-lg md:w-1/2 w-full text-center font-semibold text-white px-10 py-2 rounded-lg'>
+                        Finish
+                    </Link>
 
-                        <button onClick={() => {
-                            props.setRidePopupPanel(false)
-                            props.setConfirmRidePopupPanel(false)
-                        }}
-                            className='bg-red-500 text-lg font-semibold md:w-1/2  w-full text-white px-10 py-2 rounded-lg'>Cancel</button>
-                    </form>
+                    <p className='mt-10 text-xs text-center '>
+                        click on finish ride if you have completed the payment.
+                    </p>
+
                 </div>
 
             </div>
@@ -87,7 +73,4 @@ const ConfirmRidePopup = (props) => {
     )
 }
 
-export default ConfirmRidePopup
-
-
-
+export default FinishRide
--- frontend/src/pages/CaptainRiding.jsx
+++ frontend/src/pages/CaptainRiding.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import { Link } from 'react-router-dom'
+import FinishRide from '../components/FinishRide'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 
-const CaptainRiding = () => {
+const CaptainRiding = (props) => {
+
+  const [finishRidePanel, setFinishRidePanel] = useState(false)
+
+  const finishRidePanelRef = useRef(null)
+
+  useGSAP(() => {
+    if (finishRidePanel) {
+      gsap.to(finishRidePanelRef.current, {
+        transform: 'translateY(0)',
+        duration: 0.5,
+        ease: 'power2.out',
+      })
+    } else {
+      gsap.to(finishRidePanelRef.current, {
+        transform: 'translateY(100%)',
+        duration: 0.5,
+        ease: 'power2.out',
+      })
+    }
+  }, [finishRidePanel])
+
+
   return (
     <div className='h-screen relative '>
 
@@ -17,14 +42,24 @@ const CaptainRiding = () => {
         <img className='h-full w-full object-cover' src="https://simonpan.com/wp-content/themes/sp_portfolio/assets/uber-challenge.jpg" alt="" />
       </div>
 
-      <div className='h-1/5 p-6 flex items-center justify-between relative bg-yellow-400'>
+      <div 
+      onClick={() => {
+        setFinishRidePanel(true)
+      }}
+      className='h-1/5 p-6 flex items-center justify-between relative bg-yellow-400'>
 
         <h5 onClick={() => {
-
+          setFinishRidePanel(true)
         }} className='p-1 text-center absolute top-2 right-4 w-[93%]'> <i className="ri-arrow-up-wide-line text-3xl text-gray-900 "></i></h5>
 
         <h4 className='text-xl font-semibold'>4 KM away</h4>
         <button className='bg-green-500 font-semibold text-white px-6 py-3 rounded-lg'>Complete Ride</button>
+      </div>
+
+      <div
+        ref={finishRidePanelRef}
+        className='fixed z-10 w-full bottom-0 translate-y-full px-3 py-10 pt-12 bg-white'>
+        <FinishRide setFinishRidePanel={setFinishRidePanel} />
       </div>
 
     </div>
