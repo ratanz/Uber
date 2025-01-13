9a03cc09feb65b48d88f38a6fbac6aaa969d59a0
ratanz <ratanrathod870@gmail.com>
2025-01-13 18:24:54 +0530

    backend maps api testing v1

--- Backend/app.js
+++ Backend/app.js
@@ -8,6 +8,8 @@ const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 const cookieParser = require("cookie-parser");
 const captainRoutes = require("./routes/captain.routes");
+const mapsRoutes = require("./routes/maps.routes");
+const rideRoutes = require("./routes/ride.routes");
 
 connectToDb();
 
@@ -20,6 +22,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send("Hello Pookie");
 })
@@ -27,4 +30,8 @@ app.get('/', (req, res) => {
 app.use('/users', userRoutes);
 
 app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
+
+
 module.exports = app;
